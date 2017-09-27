f638b9c65a05d5da68be7970a73942b42a92b38e
Martin Hernandez <martin.dogg1@gmail.com>
2017-09-27 03:32:20 +0000

    Finished Lab 2

---
+++ assignments/2/new_cat.cpp
@@ -0,0 +1,60 @@
+/*
+ * Name        : New Cat
+ * Author      : Martin Hernandez
+ * Description : Program to concatenate files
+ */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstring>
+using std::cout;
+using std::ifstream;
+using std::endl;
+using std::string;
+using std::cin;
+
+int main(int argc, char *argv[]) {
+  ifstream fin;
+  string filename;
+  string str;
+  bool fileSuccess = true;
+  string noFile;
+  if (argc == 1) {
+    while (!cin.eof()) {
+      getline(cin, noFile);
+      cout << noFile << endl;;
+      if (cin.eof()) {
+        return true;
+      }
+    }
+  } else {
+  for (int i = 1; i < argc; i++) {
+    filename = argv[i];
+    fin.open(filename.c_str());
+    if (fin.is_open()) {
+      fin.close();
+  } else {
+      cout<< "File Does Not Exist" << endl;
+      fileSuccess = false;
+      return 1;
+    }
+  }
+  if (fileSuccess == true) {
+    for (int i = 1; i < argc; i++) {
+      filename = argv[i];
+      fin.open(filename.c_str());
+    while (!(fin.eof())) {
+      getline(fin, str);
+      cout << str << endl;
+      if (fin.eof()) {
+        break;
+      }
+    }
+    fin.close();
+    }
+  }
+  }
+}
+
