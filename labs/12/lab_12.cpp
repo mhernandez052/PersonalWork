b9feedb2bd45c28c44c0c43d5e0522603118149f
Martin Hernandez <martin.dogg1@gmail.com>
2017-10-05 21:16:44 +0000

    Finished Lab 12

--- labs/12/lab_12.cpp
+++ labs/12/lab_12.cpp
@@ -15,7 +15,11 @@ using std::string;
  * @param unsigned int size - The desired size of the dynamic array
  * @return int* - A pointer to the newly allocated integer array
  */
-int* MakeDynoIntArray(unsigned int size);
+int* MakeDynoIntArray(unsigned int size) {
+  int *array;
+  array = new int[size];
+  return array;
+}
 
 /*
  * Compute the sum of an array.
@@ -25,8 +29,17 @@ int* MakeDynoIntArray(unsigned int size);
  * @throw The message "NULL ARRAY REFERENCE" if the_array is NULL
  *        Syntax: throw "The Message to throw";
  */
-int Sum(int* the_array, unsigned int array_size);
-
+int Sum(int* the_array, unsigned int array_size) {
+  int sum = 0;
+if (the_array == NULL) {
+  throw "NULL ARRAY REFERENCE";
+  } else {
+      for (unsigned int i = 0; i < array_size; i++) {
+        sum = sum + the_array[i];
+      }
+    }
+  return sum;
+}
 /*
  * Identify the max value in an array.
  * @param int* the_array - The array for which the max value will be identified
@@ -35,8 +48,19 @@ int Sum(int* the_array, unsigned int array_size);
  * @throw The message "NULL ARRAY REFERENCE" if the_array is NULL
  *        Syntax: throw "The Message to throw";
  */
-int Max(int* the_array, unsigned int array_size);
-
+int Max(int* the_array, unsigned int array_size) {
+  if (the_array == NULL) {
+    throw "NULL ARRAY REFERENCE";
+  } else {
+    int max = the_array[0];
+    for (unsigned int i = 0; i < array_size; i++) {
+      if ( max < the_array[i] ) {
+        max = the_array[i];
+      }
+    }
+  return max;
+    }
+}
 /*
  * Identify the min value in an array.
  * @param int* the_array - The array for which the min value will be identified
@@ -45,7 +69,19 @@ int Max(int* the_array, unsigned int array_size);
  * @throw The message "NULL ARRAY REFERENCE" if the_array is NULL
  *        Syntax: throw "The Message to throw";
  */
-int Min(int* the_array, unsigned int array_size);
+int Min(int* the_array, unsigned int array_size) {
+  if (the_array == NULL) {
+    throw "NULL ARRAY REFERENCE";
+  } else {
+      int min = the_array[0];
+      for (unsigned int i = 0; i < array_size; i++) {
+        if ( min > the_array[i] ) {
+          min = the_array[i];
+        }
+      }
+  return min;
+  }
+}
 
 // For testing (DO NOT ALTER)
 #include <cctype>
